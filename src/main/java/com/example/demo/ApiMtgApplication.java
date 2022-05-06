e43ef15c0a4cef50c348c628a0e47886b91f3f95
vinnieoh <viniciusoliveira926@gmail.com>
2022-05-06 16:16:52 -0300

    Frist Commit

---
+++ src/main/java/com/example/demo/ApiMtgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMtgApplication.class, args);
+	}
+
+}
